239deace655283c3fc6208480b06360b716499ce
Janidu Lankage <janidulnkg1@gmail.com>
2023-04-21 17:40:37 +0530

    commit changes

--- HumanResourceManagement/HumanResourceManagement/Login.Designer.cs
+++ HumanResourceManagement/HumanResourceManagement/Login.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Dungeon", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Location = new System.Drawing.Point(47, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(604, 36);
             this.label1.TabIndex = 0;
@@ -76,7 +76,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Elephant", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(241, 112);
+            this.label2.Location = new System.Drawing.Point(331, 131);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(120, 42);
             this.label2.TabIndex = 2;
