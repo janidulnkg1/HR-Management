6595d86e80f6cd6029fae8a103d14f6fd5e7ba98
janidulnkg1 <janidulnkg1@gmail.com>
2023-07-02 09:08:39 +0530

    add UserController

--- HumanResourceManagement/HRMService/Models/User.cs
+++ HumanResourceManagement/HRMService/Models/User.cs
@@ -1,17 +1,80 @@
-﻿namespace HRMService.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRMService.Models
 {
-    public class User
-    {
-        public int userId { get; set; }
+   
+        public class User
+        {
 
-        public string FirstName { get; set; }
+            public int userId { get; set; }
 
-        public string LastName { get; set; }
 
-        public string username { get; set; }
+            public string firstName { get; set; }
 
-        public string password { get; set; }
 
-        public string Designation { get; set; }
+            public string lastName { get; set; }
+
+
+            [EmailAddress]
+
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+
+            public string Password { get; set; }
+
+            [Required]
+
+            public string Designation { get; set; }
+        }
+
+        public class UserLoginModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+
+        }
+
+        [Table("users")]
+        public class UserRegisterModel
+        {
+            [Key]
+            [Column("userID")]
+            public int userId { get; set; }
+
+
+            [Column("firstName")]
+            [Required]
+            public string firstName { get; set; }
+
+
+            [Column("lastName")]
+            [Required]
+            public string lastName { get; set; }
+
+
+            [EmailAddress]
+            [Required]
+            [Column("email")]
+            public string Email { get; set; }
+
+
+            [DataType(DataType.Password)]
+            [Required]
+            [Column("password")]
+            public string Password { get; set; }
+
+
+            [Column("designation")]
+            [Required]
+            public string Designation { get; set; }
+        }
     }
-}
